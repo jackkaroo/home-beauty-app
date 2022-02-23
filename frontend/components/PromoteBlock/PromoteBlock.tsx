029e2ff56c1564b06e6b8ed600ed231b7cd8544c
Horulia Kateryna <kateryna.horulia@gmail.com>
2022-02-23 21:02:41 +0200

    design updates

--- frontend/components/PromoteBlock/PromoteBlock.tsx
+++ frontend/components/PromoteBlock/PromoteBlock.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import Image from 'next/image';
+
+import PromoteImg from 'assets/promote_image1.jpeg';
+import PromoteImg2 from 'assets/promote_image2.jpeg';
 import styles from 'components/PromoteBlock/promote_block.module.scss';
 
 interface Props {
   isReverse?: boolean;
 }
 
-const PromoteBlock: React.FC<Props> = ({isReverse}: Props) => {
+const PromoteBlock: React.FC<Props> = ({ isReverse }: Props) => {
   return (
     <section className={isReverse ? styles.wrapper_right : styles.wrapper}>
-      <div className={styles.image}/>
+      <div className={isReverse ? styles.image_right : styles.image}>
+        <Image src={isReverse ? PromoteImg : PromoteImg2} alt="" />
+      </div>
       <div className={styles.text_wrapper}>
         <div className={styles.title}>
           Got your business?
-          <br/>
+          <br />
           {' '}
           Manage it with HomeBeauty
         </div>
